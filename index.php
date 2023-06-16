844d8dfca6ed7cdec0b6b861699b1da45b2d6461
Enyimber <Enyimber1999@gmail.com>
2023-06-16 03:19:17 +0000

    agregar estilos.css y modificar index.php

--- index.php
+++ index.php
@@ -3,9 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./estilos.css">
     <title>inventario</title>
 </head>
 <body>
-    
+    <div class="container">
+            <header>
+                <h1>Hola mundo</h1>
+            </header>
+   
+        <section>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit alias, iure vitae dignissimos natus reprehenderit fuga, hic numquam facilis  veniam itaque. Sint nesciunt doloribus quibusdam id nemo, autem molestias fugit.
+        </section>
+        <footer>
+
+        </footer>
+    </div>
 </body>
 </html>
